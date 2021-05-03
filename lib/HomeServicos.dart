ad752b4b6b552f0f362e24546b345bc409f52ae7
Diogenes <diogenesdesouza@yahoo.com.br>
2021-05-03 18:30:48 -0300

    app base done

---
+++ lib/HomeServicos.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+class HomeServicos extends StatefulWidget {
+  @override
+  _HomeServicosState createState() => _HomeServicosState();
+}
+
+class _HomeServicosState extends State<HomeServicos> {
+  @override
+  Widget build(BuildContext context) {
+    String _bodyText =
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales luctus erat, in condimentum diam.\n\n bibendum at.Mauris ultrices rutrum nulla vel consectetur. Quisque ut risus eu mauris pharetra vestibulum sed porttitor justo.\n\n Integer feugiat lorem eu enim efficitur tempus. Aliquam a fringilla neque, pellentesque iaculis neque. In ut leo a purus venenatis aliquam id a odio. Nullam luctus fringilla nunc sit amet tempus. Mauris lectus odio, rhoncus ut ultrices et, malesuada vitae ante. Integer a feugiat eros.Nulla a neque et eros dignissim tempor. Aliquam pretium porta eros in bibendum. Aenean nec libero consectetur, vestibulum diam sed, convallis arcu. Nullam ante arcu, commodo quis felis eu, fermentum dictum tellus. Sed venenatis erat eget fringilla ultricies. Duis ut risus ac metus porttitor blandit id quis lectus. Morbi imperdiet sem eu purus suscipit, vitae luctus ex pellentesque.\n\n Nam ornare, nibh vitae volutpat elementum, justo lacus luctus libero, vitae lacinia neque mauris vitae augue. In nisi nisl, vehicula eu lacinia at, tristique at augue. Duis semper porttitor viverra.Duis a ante ut nisi volutpat vestibulum. Maecenas euismod metus vel sem laoreet, ut rutrum odio pharetra. Ut et mattis nibh. Quisque fermentum felis rutrum ex fermentum, in varius quam cursus. Vivamus facilisis tellus eget quam porttitor vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec interdum nec eros ut placerat. Proin mi nisi, aliquet eget placerat id, ornare at ex. Cras ac diam erat.Curabitur ornare, augue aliquet semper pulvinar, mi lacus sagittis quam, eu semper diam augue eu turpis. Curabitur iaculis nibh ac egestas condimentum. Nunc aliquet lectus at magna lobortis, quis tincidunt purus tincidunt. Nunc pulvinar efficitur feugiat. \n\nMorbi cursus sollicitudin massa, at ultrices elit blandit eget. Sed vestibulum mi dolor, in rutrum tortor vulputate eu. Pellentesque auctor odio sit amet finibus congue. Etiam ac sem ut turpis laoreet lacinia. Nunc non dui ligula. Suspendisse potenti.In maximus fringilla elit. Vestibulum at sollicitudin dui. Vestibulum vel pellentesque massa. Vestibulum sed lectus felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum laoreet enim, sed interdum lectus porta nec. Morbi non dolor et enim accumsan tristique id nec felis. Aliquam tempor eu eros sed maximus. Aliquam quis dui ut leo vestibulum egestas. Integer non nunc ullamcorper, egestas augue a, eleifend lacus. Curabitur tincidunt, lorem id interdum maximus, eros purus varius dolor, sit amet egestas nulla sapien quis purus. Duis dui neque, congue quis gravida a, rutrum vitae lacus. Sed et sollicitudin orci. Fusce eget erat in diam hendrerit rhoncus imperdiet ac enim.";
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sodales luctus erat, in condimentum diam.\n\n bibendum at.Mauris ultrices rutrum nulla vel consectetur. Quisque ut risus eu mauris pharetra vestibulum sed porttitor justo.\n\n Integer feugiat lorem eu enim efficitur tempus. Aliquam a fringilla neque, pellentesque iaculis neque. In ut leo a purus venenatis aliquam id a odio. Nullam luctus fringilla nunc sit amet tempus. Mauris lectus odio, rhoncus ut ultrices et, malesuada vitae ante. Integer a feugiat eros.Nulla a neque et eros dignissim tempor. Aliquam pretium porta eros in bibendum. Aenean nec libero consectetur, vestibulum diam sed, convallis arcu. Nullam ante arcu, commodo quis felis eu, fermentum dictum tellus. Sed venenatis erat eget fringilla ultricies. Duis ut risus ac metus porttitor blandit id quis lectus. Morbi imperdiet sem eu purus suscipit, vitae luctus ex pellentesque.\n\n Nam ornare, nibh vitae volutpat elementum, justo lacus luctus libero, vitae lacinia neque mauris vitae augue. In nisi nisl, vehicula eu lacinia at, tristique at augue. Duis semper porttitor viverra.Duis a ante ut nisi volutpat vestibulum. Maecenas euismod metus vel sem laoreet, ut rutrum odio pharetra. Ut et mattis nibh. Quisque fermentum felis rutrum ex fermentum, in varius quam cursus. Vivamus facilisis tellus eget quam porttitor vehicula. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec interdum nec eros ut placerat. Proin mi nisi, aliquet eget placerat id, ornare at ex. Cras ac diam erat.Curabitur ornare, augue aliquet semper pulvinar, mi lacus sagittis quam, eu semper diam augue eu turpis. Curabitur iaculis nibh ac egestas condimentum. Nunc aliquet lectus at magna lobortis, quis tincidunt purus tincidunt. Nunc pulvinar efficitur feugiat. \n\nMorbi cursus sollicitudin massa, at ultrices elit blandit eget. Sed vestibulum mi dolor, in rutrum tortor vulputate eu. Pellentesque auctor odio sit amet finibus congue. Etiam ac sem ut turpis laoreet lacinia. Nunc non dui ligula. Suspendisse potenti.In maximus fringilla elit. Vestibulum at sollicitudin dui. Vestibulum vel pellentesque massa. Vestibulum sed lectus felis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Sed elementum laoreet enim, sed interdum lectus porta nec. Morbi non dolor et enim accumsan tristique id nec felis. Aliquam tempor eu eros sed maximus. Aliquam quis dui ut leo vestibulum egestas. Integer non nunc ullamcorper, egestas augue a, eleifend lacus. Curabitur tincidunt, lorem id interdum maximus, eros purus varius dolor, sit amet egestas nulla sapien quis purus. Duis dui neque, congue quis gravida a, rutrum vitae lacus. Sed et sollicitudin orci. Fusce eget erat in diam hendrerit rhoncus imperdiet ac enim.";
+
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: AppBar(
+          title: Text(
+            "Serviços",
+            style: TextStyle(fontStyle: FontStyle.italic, color: Colors.white),
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Container(
+            padding: EdgeInsets.all(30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    Image.asset("imagens/detalhe_servico.png"),
+                    Text(
+                      "Nossos Serviços",
+                      style: TextStyle(
+                          fontSize: 20,
+                          fontStyle: FontStyle.italic,
+                          color: Colors.cyan),
+                    )
+                  ],
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 30),
+                  child: Text(_bodyText,
+                      style: TextStyle(color: Colors.grey),
+                      textAlign: TextAlign.justify),
+                )
+              ],
+            ),
+          ),
+        ));
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,10 @@ import 'Home.dart';
 void main() {
   runApp(MaterialApp(
     home: Home(),
+    theme: ThemeData(
+      primarySwatch: Colors.green,
+      visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
     debugShowCheckedModeBanner: false,
   ));
 }
